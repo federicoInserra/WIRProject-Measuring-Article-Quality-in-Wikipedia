d6d5ebbdb79cb58c48b8632cf392e0053172c64a
Flavia Masoni <flavia_ma@hotmail.it>
2020-07-22 20:02:09 +0200

    naive_class.py modified

--- naive_class.py
+++ naive_class.py
@@ -2,13 +2,28 @@ import metrics
 import file_utilities as fut
 import statistics as stat
 import metrics
+import json
+
 def naive_classifier():
     countries = fut.get_countries()
+    country_list = []
+    country_dict = dict()
+    for country in countries[:5]:
+        print(f"{country}")
+        revisions = fut.get_revisions(country)
+        max_length = 0
+        for revision in revisions[:50]:
+            text_len = len(fut.parse_text(revision["text"]).split())
+            if(text_len > max_length):
+                max_length = text_len
+        country_dict[country] = max_length
+        finale = sorted(country_dict, key=country_dict.get, reverse=True)
+    return finale
 
-    for country in countries:
 
-        revisions = stat.get_revisions(country)
-        print(revisions["text"])
-        break
 if __name__ == "__main__":
-    naive_classifier()
+    f = open("ranked_countries.json", "r", encoding="utf-8")
+    data = json.load(f)
+    ordered_list = naive_classifier()
+    metrics.NDCG_score(data,ordered_list,81)
+    f.close()
