e28fc5e67d68a5bf209ce8725be284d966bacc2e
Leonardo Razovic <leorazzo@gmail.com>
2020-07-22 10:57:16 +0200

    articles_per_author

--- statistics.py
+++ statistics.py
@@ -1,18 +1,22 @@
 from file_utilities import decompress_pickle, get_countries
 from pathlib import Path
+from typing import Dict
 
 
 def average(lst):
     return sum(lst) / len(lst)
 
 
-if __name__ == "__main__":
-    countries = get_countries()
+def get_revisions(country: str):
+    path = f"countries/{country.lower()}"
+    Path(path).mkdir(parents=True, exist_ok=True)
+    return decompress_pickle(f"{path}/revisions.pbz2")
+
+
+def authors_per_article(countries: list):
     avg_authors = list()
     for country in countries:
-        path = f"countries/{country.lower()}"
-        Path(path).mkdir(parents=True, exist_ok=True)
-        revisions = decompress_pickle(f"{path}/revisions.pbz2")
+        revisions = get_revisions(country)
         authors = list()
         for revision in revisions:
             user = revision["user"]
@@ -28,7 +32,34 @@ if __name__ == "__main__":
     print(
         f"Authors per article: min {min(avg_authors)} max: {max(avg_authors)} avg: {average(avg_authors)}"
     )
-    no_duplicates = set(avg_authors)
-    print(
-        f"Unique authors per article: min {min(no_duplicates)} max: {max(no_duplicates)} avg: {average(no_duplicates)}"
-    )
+
+
+def articles_per_author(countries: list):
+    authors: Dict[str, int] = dict()
+    for country in countries:
+        print(country)
+        revisions = get_revisions(country)
+        for revision in revisions:
+            user = revision["user"]
+            if user in authors:
+                authors[user] += 1
+            else:
+                authors[user] = 1
+    all_values = authors.values()
+    avg_value = average(all_values)
+    max_value = max(all_values)
+    min_value = min(all_values)
+    print(max_value, min_value, avg_value)
+    print("No Anon")
+    del authors["Anonimous"]
+    all_values = authors.values()
+    avg_value = average(all_values)
+    max_value = max(all_values)
+    min_value = min(all_values)
+    print(max_value, min_value, avg_value)
+
+
+if __name__ == "__main__":
+    countries = get_countries()
+    # authors_per_article(countries)
+    # articles_per_author(countries)
